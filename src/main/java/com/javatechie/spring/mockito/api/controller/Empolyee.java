8c69fb95453ec6d2ce6c71add0e0d1250891c609
romipraveen1 <romipraveen100@gmail.com>
2019-07-25 16:08:04 +0530

    test jenkins3

--- src/main/java/com/javatechie/spring/mockito/api/controller/Empolyee.java
+++ src/main/java/com/javatechie/spring/mockito/api/controller/Empolyee.java
@@ -3,6 +3,14 @@ package com.javatechie.spring.mockito.api.controller;
 public class Empolyee {
 	
 	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 	
 
 }
